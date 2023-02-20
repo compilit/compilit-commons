2315e4ccd95d6d018990fc0610d86450f2094e8e
HarveyTheRabbit <info@compilit.com>
2023-02-20 11:15:14 +0100

    refactor: mada DomainPrimitiveException public with private ctors

--- domain-primitives/src/main/java/com/compilit/domainprimitives/DomainPrimitiveException.java
+++ domain-primitives/src/main/java/com/compilit/domainprimitives/DomainPrimitiveException.java
@@ -1,12 +1,12 @@
 package com.compilit.domainprimitives;
 
-final class DomainPrimitiveException extends RuntimeException {
+public final class DomainPrimitiveException extends RuntimeException {
 
-  public <T> DomainPrimitiveException(String domainPrimitiveName, T value) {
+  <T> DomainPrimitiveException(String domainPrimitiveName, T value) {
     super(String.format("Provided value '%s' is not a valid %s", value, domainPrimitiveName));
   }
 
-  public <T> DomainPrimitiveException(String domainPrimitiveName, T value, String message) {
+  <T> DomainPrimitiveException(String domainPrimitiveName, T value, String message) {
     super(String.format("Provided value '%s' is not a valid %s: %s", value, domainPrimitiveName, message));
   }
 
